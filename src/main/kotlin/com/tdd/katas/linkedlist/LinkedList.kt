0287ae2662b59a0ecdc7188eb4fa973b2ea1834d
XYZ <idm.caz@mailinator.com>
2017-03-25 12:56:11 +0100

    New test: "When adding an element the size should not be zero" Implementation

--- src/main/kotlin/com/tdd/katas/linkedlist/LinkedList.kt
+++ src/main/kotlin/com/tdd/katas/linkedlist/LinkedList.kt
@@ -2,17 +2,19 @@ package com.tdd.katas.linkedlist
 
 class LinkedList {
 
-    private var empty : Boolean = true;
+    private var empty : Boolean = true
+    private var size : Int = 0
 
     fun  isEmpty(): Boolean {
         return empty
     }
 
     fun  size(): Int {
-        return 0
+        return size
     }
 
     fun  add(element: String) {
+        size++
         empty = false
     }
 
