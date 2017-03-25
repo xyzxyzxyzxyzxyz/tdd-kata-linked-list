8388145d4994e57c0f84f0240c55b130b93b033b
XYZ <idm.caz@mailinator.com>
2017-03-25 17:37:34 +0100

    New test: When an element has not been added, the contains() method should return false Implementation refactoring: Changing to a linked list structure

--- src/main/kotlin/com/tdd/katas/linkedlist/LinkedList.kt
+++ src/main/kotlin/com/tdd/katas/linkedlist/LinkedList.kt
@@ -2,9 +2,11 @@ package com.tdd.katas.linkedlist
 
 class LinkedList {
 
-
-    private var elements : Array<String?> = arrayOfNulls(10)
+    private var elements : Array<Node?> = arrayOfNulls(10)
     private var size : Int = 0
+
+    private var firstIndex : Int? = null
+    private var lastIndex : Int? = null
 
     fun isEmpty(): Boolean {
         return size==0
@@ -15,8 +17,30 @@ class LinkedList {
     }
 
     fun add(element: String) {
-        elements[size] = element
-        size++
+        if (isEmpty()) {
+            val node : Node = Node(element, null, null)
+
+            val nodeIndex = size;
+
+            elements[nodeIndex] = node
+            firstIndex = nodeIndex
+            lastIndex = nodeIndex
+
+            size++
+        }
+        else {
+            val lastNode = elements[lastIndex!!]
+
+            val nodeIndex = size;
+
+            lastNode!!.next = nodeIndex
+
+            val node : Node = Node(element, lastIndex, null)
+            elements[nodeIndex] = node
+            size++
+
+            lastIndex = nodeIndex
+        }
     }
 
     fun clear() {
@@ -24,13 +48,26 @@ class LinkedList {
     }
 
     fun contains(element: String): Boolean {
-        for (i in 0..size) {
-            if (elements[i] == element) {
+        for (i in 0 .. size-1) {
+            if (elements[i]!!.value == element) {
                 return true
             }
         }
 
         return false
+    }
+
+
+    class Node {
+        constructor(value: String?, previous: Int?, next: Int?) {
+            this.value = value
+            this.previous = previous
+            this.next = next
+        }
+
+        var value : String? = null
+        var previous : Int? = null
+        var next : Int? = null
     }
 
 }
